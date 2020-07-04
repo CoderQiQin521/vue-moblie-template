5a78485ff4eb9060aeaae525d4c05cc83936cd61
CoderQiQin521 <coderqiqin@aliyun.com>
2020-07-04 11:40:50 +0800

    导航守卫设置页面title

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,41 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "Home",
+    meta: {
+      title: "首页"
+    },
+    component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
+    meta: {
+      title: "关于我们"
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
+});
+
+router.beforeEach((to, from, next) => {
+  let { title } = to.meta;
+  title && (document.title = title);
+  next();
 });
 
 export default router;
