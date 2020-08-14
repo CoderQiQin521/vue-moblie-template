4f2f03a5376f45297de59375c3466e792141aa4c
CoderQiQin521 <coderqiqin@aliyun.com>
2020-08-14 16:10:14 +0800

    文件头说明

--- src/global/config.js
+++ src/global/config.js
@@ -1,3 +1,10 @@
+/*
+ * 项目配置参数
+ * @Author: coderqiqin@aliyun.com
+ * @Date: 2020-08-14 16:08:45
+ * @Last Modified by:   CoderQiQin
+ * @Last Modified time: 2020-08-14 16:08:45
+ */
 export const mockServer = true; // 是否请求mock服务
 export const mockHost = "http://localhost:3000";
 export const openCrypto = true; // 是否参数加密
--- src/global/const.js
+++ src/global/const.js
@@ -1,9 +1,9 @@
 /*
- * 全局常量
+ * 业务全局常量
  * @Author: coderqiqin@aliyun.com
  * @Date: 2020-08-13 19:32:53
  * @Last Modified by: CoderQiQin
- * @Last Modified time: 2020-08-13 22:40:35
+ * @Last Modified time: 2020-08-14 16:09:14
  */
 const ACTIVE_NAME = "超时宝";
 const MAX_MONEY = 20000;
